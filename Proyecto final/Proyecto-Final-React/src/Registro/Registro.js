1e1914fcf6dee1ccbc0b06c1ba5f1ab2234222c5
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-22 10:21:21 -0300

    nada misma

--- Proyecto final/Proyecto-Final-React/src/Registro/Registro.js
+++ Proyecto final/Proyecto-Final-React/src/Registro/Registro.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, Card, ListGroup  } from 'react-bootstrap';
 import './Registro.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import "./Registro.css"; 
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [numero, setNumero] = useState('');
+  const [pais, setPais] = useState('');
+  const [ciudad, setCiudad] = useState('');
   const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
@@ -48,12 +54,26 @@ function Login() {
   };
 
   return (
+    
     <Container className="login-container">
+      
       <Row className="justify-content-center align-items-center" style={{ minHeight: '100vh', marginTop:'-85px' }}>
         <Col xs={12} md={6} lg={4}>
           <div className="login-form">
             <h2 className="text-center mb-4">Registrate</h2>
             <Form>
+            <Form.Group controlId="nombre">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" value={nombre} onChange={handleEmailChange} />
+              </Form.Group>
+              <Form.Group controlId="apellido">
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control type="text" value={apellido} onChange={handleEmailChange} />
+              </Form.Group>
+              <Form.Group controlId="numero">
+                <Form.Label>Numero</Form.Label>
+                <Form.Control type="text" value={numero} onChange={handleEmailChange} />
+              </Form.Group>
               <Form.Group controlId="email">
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" value={email} onChange={handleEmailChange} />
@@ -63,10 +83,14 @@ function Login() {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" value={password} onChange={handlePasswordChange} />
               </Form.Group>
-
-              <Button variant="primary" block onClick={handleLogin}>
-                Iniciar Sesión
-              </Button>
+              <Form.Group controlId="pais">
+                <Form.Label>Pais</Form.Label>
+                <Form.Control type="text" value={pais} onChange={handleEmailChange} />
+              </Form.Group>
+              <Form.Group controlId="ciudad">
+                <Form.Label>Ciudad</Form.Label>
+                <Form.Control type="text" value={ciudad} onChange={handleEmailChange} />
+              </Form.Group>
               <Button variant="primary" block onClick={HandleRegister}>
                 Registrarse
               </Button>
@@ -74,7 +98,9 @@ function Login() {
           </div>
         </Col>
       </Row>
+      
     </Container>
+    
   );
 }
 
