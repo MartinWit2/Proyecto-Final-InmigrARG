d6ddad1854ecd711e56f5c239349ad5a115609d3
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-09 10:35:38 -0300

    home

--- Proyecto final/Proyecto-Final-React/src/Home/Home.js
+++ Proyecto final/Proyecto-Final-React/src/Home/Home.js
@@ -6,8 +6,9 @@ function Home(){
         <div className="left-side"></div>
         <div className="right-side">
           <div className="intro-text">
-            <h1>¡InmigrARG!</h1>
-            <p>Este es un texto introductorio que puedes personalizar según tus necesidades. Puedes agregar más contenido aquí.</p>
+            <h1>InmigrARG</h1>
+            <h2>¿Quienes somos?</h2>
+            <p>Somos una empresa que busca brindar asistencia y asesoramiento a inmigrantes para poder establecerse y adaptarse dentro de Argentina cumpliendo con las gestiones necesarias para garantizar su bienestar. Nuestro objetivo es ser el servicio que toda persona considere como primera opción al tomar la decisión de emigrar.</p>
           </div>
         </div>
       </div>
